534d1d4a0170099e0531bf7318c8501c759ed31d
maryanionutz <maryanionutz93@yahoo.com>
2023-07-06 21:13:40 +0300

    Updated

---
+++ Teme/Methods/Tema1/src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+class Methods {
+    int value;
+    String animal;
+    int days;
+
+    void run() {
+        System.out.println("My " + animal + " is " + value + " years old.");
+    }
+
+    void calculateMyBirthday() {
+        int daysLeft = 20 + days;
+        System.out.println("Days until my birthday " + daysLeft);
+    }
+
+    void talk(String text) {
+
+        System.out.println(text);
+    }
+
+    void speed(int minutes, double seconds) {
+        System.out.println("I get down the hill in " + minutes + " and " + seconds);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Methods name = new Methods();
+        name.value = 4;
+        name.animal = "Cat";
+        name.days = 8;
+        name.run();
+        name.calculateMyBirthday();
+        name.talk("Hi, my name is John");
+        name.speed(4, 4.5);
+
+    }
+
+}
