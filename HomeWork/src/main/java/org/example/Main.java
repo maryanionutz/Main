c6776ff4a3b8136502281906058e839fd2b8b284
maryanionutz <maryanionutz93@yahoo.com>
2023-07-06 14:45:57 +0300

    Update

--- HomeWork/src/main/java/org/example/Main.java
+++ HomeWork/src/main/java/org/example/Main.java
@@ -1,32 +1,39 @@
-package org.example;
+package HomeWork.src.main.java.org.example;
 class Methods {
     int value;
     String animal;
     int days;
-
     void run() {
-        System.out.println("My "+animal+ " is "+value+ " years old.");
+        System.out.println("My " + animal + " is " + value + " years old.");
     }
-    void calculateMyBirthday(){
+
+    void calculateMyBirthday() {
         int daysLeft = 20 + days;
-        System.out.println("Days until my birthday "+daysLeft);
+        System.out.println("Days until my birthday " + daysLeft);
     }
-    void talk (String text){
+
+    void talk(String text) {
+
         System.out.println(text);
     }
-}
-public class Main {
-    public static void main(String[] args) {
-        Methods name = new Methods();
-        name.value = 4;
-        name.animal = "Cat";
-        name.days = 8;
-        name.run();
-        name.calculateMyBirthday();
-        name.talk("Hi, my name is John");
 
-
+    void speed(int minutes, double seconds) {
+        System.out.println("I get down the hill in "+minutes+ " and "+seconds);
     }
 
+    public class Main {
+        public static void main(String[] args) {
+            Methods name = new Methods();
+            name.value = 4;
+            name.animal = "Cat";
+            name.days = 8;
+            name.run();
+            name.calculateMyBirthday();
+            name.talk("Hi, my name is John");
+            name.speed(4, 4.5);
 
+
+        }
+
+    }
 }
